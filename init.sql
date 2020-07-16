2a16aa802df983e34a5e31ec70e8c98cba1df438
RuiyuYao <ruiyuyao@bitorobotics.ltd>
2020-07-16 18:45:24 +0800

    update sql

--- init.sql
+++ init.sql
@@ -1,7 +1,20 @@
 CREATE TABLE `todolist`.`user`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `id` int(0) NOT NULL AUTO_INCREMENT PRIMARY,
   `username` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+
+CREATE TABLE `todolist`.`todo`  (
+  `id` int(0) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY,
+  `user_id` int(0) NOT NULL,
+  `detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `status` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `valid` tinyint(0) NULL DEFAULT NULL,
+  `sort` int(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`, `user_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+select * from user t where t.username = 'user'
